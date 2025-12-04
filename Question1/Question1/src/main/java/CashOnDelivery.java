b8aeb978d3c01adfe05612c49277992a8ca24e5c
M00N1C <mostafa.mcm.85@gmail.com>
2025-12-04 15:32:29 +0330

    ALL Questions are done

--- Question1/Question1/src/main/java/CashOnDelivery.java
+++ Question1/Question1/src/main/java/CashOnDelivery.java
@@ -1,2 +1,12 @@
-public class CashOnDelivery {
+public class CashOnDelivery extends Payment {
+    public CashOnDelivery(double amount, String address) {
+        super(amount);
+        this.address = address;
+    }
+    private String address;
+    @Override
+    public void processPayment() {
+        System.out.println("Cash On Delivery Payment Done in " + address);
+        logTransaction();
+    }
 }
--- Question1/Question1/src/main/java/CreditCardPayment.java
+++ Question1/Question1/src/main/java/CreditCardPayment.java
@@ -1,2 +1,21 @@
-public class CreditCardPayment {
+public class CreditCardPayment extends Payment implements SecureTransaction{
+    public CreditCardPayment(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public void processPayment() {
+        if(authenticate()){
+            System.out.println("Credit Card Payment done Successfully");
+            logTransaction();
+        }
+        else{
+            System.out.println("Credit Card Payment failed, Couldn't authenticate user");
+        }
+    }
+
+    @Override
+    public boolean authenticate() {
+        return false;
+    }
 }
