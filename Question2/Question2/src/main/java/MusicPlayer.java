30a5211227b7374b36a7edfb2518fc6d58f536f6
M00N1C <mostafa.mcm.85@gmail.com>
2025-12-04 15:01:24 +0330

    Done VideoPlayer

---
+++ Question2/Question2/src/main/java/MusicPlayer.java
@@ -0,0 +1,11 @@
+public class MusicPlayer implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Playing audio file...");
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ Question2/Question2/src/main/java/VideoPlayer.java
@@ -0,0 +1,13 @@
+public class VideoPlayer implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing video file...");
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
+
